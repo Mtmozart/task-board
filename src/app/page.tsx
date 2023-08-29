da42d44d37d1452df6559b962b4baaeabc09fc07
Mtmozart <mmsnborges@gmail.com>
2023-08-29 14:17:11 -0300

    start the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,23 @@
 import Image from "next/image";
 import HomeCapa from "../../public/assets/hero.png";
+import styles from "../styles/home.module.css";
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <Image alt="Logo da home" src={HomeCapa} priority />
-      </div>
-      <h1>
-        Sistema feito para que você organize <br /> suas tarefas
-      </h1>
-    </main>
+    <div className={styles.container}>
+      <main>
+        <div className={styles.logo}>
+          <Image
+            className={styles.hero}
+            alt="Logo da home"
+            src={HomeCapa}
+            priority
+          />
+        </div>
+        <h1 className={styles.title}>
+          Sistema feito para que você organize <br /> suas tarefas
+        </h1>
+      </main>
+    </div>
   );
 }
