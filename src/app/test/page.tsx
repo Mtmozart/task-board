db90d1cfdaca6eabe052985fdb46c7d748087f5b
Mtmozart <mmsnborges@gmail.com>
2023-08-31 20:11:32 -0300

    Creaing the header

---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+import HomeCapa from "../../public/assets/hero.png";
+import styles from "../styles/home.module.css";
+
+export default function Home() {
+  return <h1>Teste</h1>;
+}
