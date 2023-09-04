71513fc89a3fed28449ca04c06d519fb0130b777
Mtmozart <mmsnborges@gmail.com>
2023-09-04 20:59:05 -0300

    verify how user now

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -6,6 +6,12 @@ import styles from "./styles.module.css";
 import { TextArea } from "@/components/textArea/page";
 import { FiShare2 } from "react-icons/fi";
 import { FaTrash } from "react-icons/fa";
+//Database
+import { db } from "../../services/FireBaseConnection";
+import { addDoc, collection } from "firebase/firestore";
+
+//react
+import { ChangeEvent, FormEvent, useState } from "react";
 
 export const metadata: Metadata = {
   title: "My painel of tasks",
@@ -13,6 +19,28 @@ export const metadata: Metadata = {
 };
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [publicTask, setPublicTask] = useState(false);
+
+  function handleChangePublic(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.checked);
+    setPublicTask(e.target.checked);
+  }
+
+  async function handleRegisterTask(e: FormEvent) {
+    e.preventDefault();
+    if (input === "") return;
+    try {
+      await addDoc(collection(db, "tasks"), {
+        task: input,
+        created: new Date(),
+        user: "",
+        public: publicTask,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
   const { data: session } = useSession({
     required: true,
     onUnauthenticated() {
@@ -24,10 +52,21 @@ export default function Home() {
       <section className={styles.content}>
         <div className={styles.contentForm}>
           <h1 className={styles.title}>Add your task</h1>
-          <form action="">
-            <TextArea placeholder="Enter your task..." />
+          <form onSubmit={handleRegisterTask}>
+            <TextArea
+              onChange={(e: ChangeEvent<HTMLTextAreaElement>) =>
+                setInput(e.target.value)
+              }
+              value={input}
+              placeholder="Enter your task..."
+            />
             <div className={styles.checkBoxArea}>
-              <input type="checkbox" className={styles.checkBox} />
+              <input
+                type="checkbox"
+                className={styles.checkBox}
+                checked={publicTask}
+                onChange={handleChangePublic}
+              />
               <label>Publish the task?</label>
             </div>
             <button type="submit" className={styles.button}>
---
+++ src/services/FireBaseConnection.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
+  projectId: process.env.PROJECTID,
+  storageBucket: process.env.STORAGEBUCKET,
+  messagingSenderId: process.env.MESSAGINGSENDERID,
+  appId: process.env.APP_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp);
+
+export { db };
